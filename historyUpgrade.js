96ab2400e5d5a68fd07050816059e1aa602a5fd6
WSchrader <wilrader@gmail.com>
2014-02-04 20:32:29 -0800

    ...

--- historyUpgrade.js
+++ historyUpgrade.js
@@ -20,54 +20,59 @@ app.configure('production', function() {
 var Address = mongoose.model('Address');
 var History = mongoose.model('History');
 
-var addresses = 0;
-var dataPoints = 0;
-var newHistDocs = [];
+// Dump the history table
+History.collection.drop(function (err) {
+	
+	log.info('Dropped history table...');
+	var addresses = 0;
+	var dataPoints = 0;
+	var newHistDocs = [];
 
-//Upgrade the entire address table
-var stream = Address.find().stream();
+	//Upgrade the entire address table
+	var stream = Address.find().stream();
 
-stream.on('data', function (address) {
-	addresses++;
-	
-	var btcAddr = address.address;
+	stream.on('data', function (address) {
+		addresses++;
 		
-	var dataLen = address.data.length;
-	
-	dataPoints += dataLen;
-	
-	for (var d = 0; d < dataLen; d++) {
-		var data = address.data[d];
+		var btcAddr = address.address;
+			
+		var dataLen = address.data.length;
 		
-		var hist = {
-			address: btcAddr,
-			createdAt: data.retrieved,
-			hashRate: data.hashRate,
-			balances: {
-				sent: data.balances.sent,
-				confirmed: data.balances.confirmed,
-				unconverted: data.balances.unconverted,
-			}
-		};
+		dataPoints += dataLen;
 		
-		newHistDocs.push(hist);
-	}
-	
-}).on('error', function (err) {
-	log.error(err);
-	disconnect();
-	
-}).on('close', function() {
-	log.info('Found %d addresses...', addressesLen);
-	log.info('Converting %d datapoints...', dataPoints);
-	log.info('Executing create call...');
-	
-	History.create(newHistDocs, function (createError) {
-		if (createError) {
-			log.err(createError);
+		for (var d = 0; d < dataLen; d++) {
+			var data = address.data[d];
+			
+			var hist = {
+				address: btcAddr,
+				createdAt: data.retrieved,
+				hashRate: data.hashRate,
+				balances: {
+					sent: data.balances.sent,
+					confirmed: data.balances.confirmed,
+					unconverted: data.balances.unconverted,
+				}
+			};
+			
+			newHistDocs.push(hist);
 		}
 		
+	}).on('error', function (err) {
+		log.error(err);
 		disconnect();
+		
+	}).on('close', function() {
+		log.info('Found %d addresses...', addresses);
+		log.info('Converting %d datapoints...', dataPoints);
+		log.info('Executing create call...');
+		
+		History.create(newHistDocs, function (createError) {
+			if (createError) {
+				log.err(createError);
+			}
+			
+			disconnect();
+		});
 	});
 });
 
