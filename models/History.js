3ccc11ce1fd37a6a257732aae5b3df3fab2e93f8
WSchrader <wilrader@gmail.com>
2014-02-10 16:31:14 -0800

    Increase expire time to 1 month

--- models/History.js
+++ models/History.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 module.exports = function() {
 	var History = new Schema({
 		address: String,
-		createdAt: {type: Date, default: Date.now, expires: 60*60*24*7 },
+		createdAt: {type: Date, default: Date.now, expires: 60*60*24*7*4 },
 		hashRate: Number,
 		balances: {
 			sent: Number,
