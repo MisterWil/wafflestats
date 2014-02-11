caa38bf2502f6f00343c2320385b6390a675e9c1
WSchrader <wilrader@gmail.com>
2014-02-11 09:01:06 -0800

    V 0.51

--- wafflestats.js
+++ wafflestats.js
@@ -22,7 +22,7 @@ var historical = require('./routes/historical')(app, rclient);
 
 app.configure(function() {
 	// Waffles Version Info
-	app.set('wafflesVersion', '0.5');
+	app.set('wafflesVersion', '0.51');
 
 	// all environments
 	app.set('port', process.env.PORT || 3000);
