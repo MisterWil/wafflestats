bd5a4926bd86e11a4d97c45cfdf25986dda8b98f
WSchrader <wilrader@gmail.com>
2014-02-16 15:13:57 -0800

    Added current hashrate to the window title.

--- public/javascripts/wafflestats.js
+++ public/javascripts/wafflestats.js
@@ -104,6 +104,7 @@ var idleTimeout = 1000 * 60;
 var hashrateFormatString = "%.2f kH/s";
 var hashrateDecimalFormatString = "%.2f";
 var bitcoinFormatString = "%.8f";
+var titleFormatString = "%.2f kH/s - WAFFLEStats";
 
 // Regex to test bitcoin addresses before pinging for current data
 // Disabled for historical data so I can test locally using historical data
@@ -638,6 +639,8 @@ function updateValues() {
 	updateHashrateMetrics();
 	
 	setValue("#hashrate", sprintf(hashrateDecimalFormatString, CURRENT_DATA.hashRate));
+	
+	document.title = sprintf(titleFormatString, CURRENT_DATA.hashRate);
 	
 	setValue("#sent", sprintf(bitcoinFormatString, CURRENT_DATA.sent));
 	setValue("#confirmed", sprintf(bitcoinFormatString, CURRENT_DATA.confirmed));
