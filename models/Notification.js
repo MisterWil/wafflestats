8fa181cc91157bed1e135a43c0f14a675a88fa2f
WSchrader <wilrader@gmail.com>
2014-02-24 17:12:45 -0800

    Forgot a file.

--- models/Notification.js
+++ models/Notification.js
@@ -13,8 +13,8 @@ module.exports = function() {
 		lastHashrateNotification: { type: Date, default: Date.now },	// When the last hashrate notification was sent out
 			
 		hashrateEnabled: { type: Boolean, default: false }, 			// Hashrate notifications enabled
-		averageMinutes: { type: Number, default: 15 },					// How long the hashrate must remain low for a notification to trigger
 		averageDays: { type: Number, default: 1 },						// Number of hours used to calculate hashrate average
+		averageMinutes: { type: Number, default: 15 },					// How long the hashrate must remain low for a notification to trigger
 		percentThreshold: { type: Number, default: 0.5 },				// Percentage difference to trigger notification
 			
 		paymentEnabled: { type: Boolean, default: false }
