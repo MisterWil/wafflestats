043a125e3d27cd648067d9b1c02b53d847a6d274
WSchrader <wilrader@gmail.com>
2014-03-19 15:39:10 -0700

    Saving worker hashrates historically now

--- models/History.js
+++ models/History.js
@@ -6,6 +6,11 @@ module.exports = function(configuration) {
 		address: { type: String },
 		createdAt: {type: Date, default: Date.now, expires: 60*60*24*7*2, index: true },
 		hashRate: Number,
+		workerHashrates: [{
+			username: { type: String },
+			hashRate: { type: Number },
+			lastSeen: { type: Date }
+		}],
 		balances: {
 			sent: Number,
 			confirmed: Number,
